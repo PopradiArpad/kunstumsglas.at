7d76cb5f0193eaedaac08d118fc4fcb009533796
Poprádi Árpád <popradi.arpad11@gmail.com>
2019-06-10 09:52:45 +0200

    Let create initial database.
    Add install instruction to doc/.
    
    Let run local app with one npm run command.
    
    Remove redundant test.
    
    Status: no simple way to store product and website data without complete app, no server test.
    
    Modularize start_test_app_local.sh.
    
    Split development from test. Make own npm run for development app start and mongo start. Update doc.
    
    Add doc how to get access to a development database content.
    
    Add doc to consider reinstall node_modules after branch change.
    
    Add doc and script to create initial KUG database.
    
    Add create_initial_db command of db_management.sh.
    
    Let create an initial db, which works after first user registered.
    
    Add how to setup main view manually :((
    
    Make db setup less manually. Error: translations can not be shown.
    
    Doc how to create initial db. Give more standard content of the initial collections.
    
    Fix missing locales setup in initial db creator.

---
+++ development/start_development_app.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+SCRIPT_CALLING_PATH="$0";
+SCRIPT_CALLING_DIR=$(dirname "$SCRIPT_CALLING_PATH");
+SCRIPT_ABS_DIR=$(realpath "${SCRIPT_CALLING_DIR}");
+PROJECT_DIR=$(realpath "${SCRIPT_ABS_DIR}/..");
+
+. "${SCRIPT_ABS_DIR}/include_local_development_environment_data.sh"
+
+RED="\e[31m"
+CYAN="\e[36m"
+NORM="\e[0m"
+
+
+function start_mongod() {
+  echo -e "${CYAN}Starting mongod${NORM}";
+  # remember the PID of mongod to let it kill
+  mongod --master --dbpath $DB_PATH --port $DB_PORT & echo $! > mongod.pid;
+}
+
+function exit_if_mongod_cannot_start() {
+  sleep .5;
+
+  if ! ps `cat mongod.pid`>/dev/null; then
+    rm mongod.pid;
+    exit 1;
+  fi
+}
+
+function run_kug_server_and_wait_for_end() {
+  echo -e "${CYAN}Starting kug server${NORM}";
+  cd "${PROJECT_DIR}";
+  PORT=$PORT MONGO_URL=mongodb://127.0.0.1:$DB_PORT/test node --trace-warnings server_build/server.js --  --log --graphiql;
+}
+
+function kill_mongod() {
+  MONGOPID=`cat mongod.pid`;
+  rm mongod.pid;
+
+  if ps $MONGOPID>/dev/null; then
+    #Never use kill -9 on mongod!
+    kill -2 $MONGOPID;
+  fi
+}
+
+
+start_mongod;
+exit_if_mongod_cannot_start;
+run_kug_server_and_wait_for_end;
+kill_mongod;
